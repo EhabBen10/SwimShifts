fe57e1e47a84eda7765f64625e0dfd12a77d2cbe
Ehabben10 <202107666@post.au.dk>
2023-11-26 16:10:10 +0100

    cannot convert 11-nov to datetime

---
+++ Shifts.Presentation.API/Controllers/ShiftsController.cs
@@ -0,0 +1,33 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Sheets.v4;
+using Microsoft.AspNetCore.Mvc;
+using Shifts.Application.Interfaces;
+using Shifts.Application.Models;
+using Shifts.Infrastructure.Sheets.Services;
+
+namespace Shifts.Presentation.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ShiftsController : ControllerBase
+{
+    public IGoogleSheetsService _googleSheetsService;
+    public IGoogleCalendarService _googleCalendarService;
+
+    public ShiftsController(IGoogleSheetsService googleSheetsService, IGoogleCalendarService googleCalendarService)
+    {
+        _googleSheetsService = googleSheetsService;
+        _googleCalendarService = googleCalendarService;
+    }
+
+    [HttpGet(Name = "getshifts")]
+    public async Task<List<Shift>> Get()
+    {
+        IList<IList<object>> objects = await _googleSheetsService.ReadDataFromGoogleSheet();
+        List<Shift> shifts =  _googleSheetsService.FindShifts(objects,"Ehab");
+        List<Event> events =  _googleCalendarService.events(shifts);
+        //  _googleCalendarService.CreateEvents(events);
+        return null;
+    }
+}
