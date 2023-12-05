3574eda29a714f3b1b940c40e149f3fcae342bb6
Ehabben10 <202107666@post.au.dk>
2023-12-05 11:55:50 +0100

    adding somthing to check the year and add a year for the datetime

--- Shifts.Presentation.API/Controllers/ShiftsController.cs
+++ Shifts.Presentation.API/Controllers/ShiftsController.cs
@@ -1,10 +1,7 @@
-using Google.Apis.Auth.OAuth2;
 using Google.Apis.Calendar.v3.Data;
-using Google.Apis.Sheets.v4;
 using Microsoft.AspNetCore.Mvc;
 using Shifts.Application.Interfaces;
 using Shifts.Application.Models;
-using Shifts.Infrastructure.Sheets.Services;
 
 namespace Shifts.Presentation.API.Controllers;
 
