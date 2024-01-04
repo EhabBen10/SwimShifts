83313c82335a624f2909ec75d396f46d3dcd168c
Ehabben10 <202107666@post.au.dk>
2024-01-04 10:21:31 +0100

    adding the import to calender mutation

---
+++ Shifts.Presentation.GraphQL/Mutations/ImportToCalender.cs
@@ -0,0 +1,18 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3.Data;
+using Shifts.Application.Models;
+
+namespace Shifts.Presentation.GraphQL.Mutations
+{
+    [ExtendObjectType("Mutation")]
+    public class ImportToCalender
+    {
+        public async Task<bool> ImportToGoogleCalenderAsync(IGoogleCalendarService _googleCalendarService, IGoogleAuthService _googleAuthService, List<Shift> shifts)
+        {
+            UserCredential credential = await _googleAuthService.AuthorizeAsync();
+            List<Event> events = _googleCalendarService.events(shifts, "fe");
+            // await _googleCalendarService.CreateEvents(events, credential);
+            return true;
+        }
+    }
+}
